3e3e68330b3d73480b6224b215ff906a95b8e24c
monles <iceueb@gmail.com>
2022-07-15 04:40:02 +0100

    video cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import styled from 'styled-components';
+import Menu from './components/Menu';
+import NavBar from './components/NavBar';
+
+const Container = styled.div`
+  display: flex;
+`;
+const Main = styled.div``;
+const Wrapper = styled.div``;
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <NavBar />
+        <Wrapper>video cards</Wrapper>
+      </Main>
+    </Container>
   );
 }
 
